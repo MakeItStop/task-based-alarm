2131caf61b145cf2882bd25a5685848d906ac5b3
Richard Bates <richo225@gmail.com>
2016-09-08 16:53:12 +0100

    Add commented out test for saveTime()

--- app/tests/setAlarm.component_spec.js
+++ app/tests/setAlarm.component_spec.js
@@ -5,25 +5,26 @@ var appSettings = require("application-settings");
 
 describe("Tests for /pages/setAlarm/setAlarm.component.ts", function() {
 
-     var setAlarmComponent;
-     beforeEach(function() {
-         setAlarmComponent = new component.SetAlarmPage();
-     });
+  var setAlarmComponent;
+  beforeEach(function() {
+     setAlarmComponent = new component.SetAlarmPage();
+  });
 
-    it("Verifies that configureTime set a default time value", function() {
+  it("Verifies that configureTime set a default time value", function() {
+    var timePicker = jasmine.createSpyObj('timePicker', ['hour', 'minute']);
+    appSettings.setNumber("hour", 9);
+    appSettings.setNumber("minute", 25);
+    setAlarmComponent.configureTime(timePicker);
+    expect(timePicker.hour).toEqual(9);
+    expect(timePicker.minute).toEqual(25);
+  });
 
-        var timePicker = jasmine.createSpyObj('timePicker', ['hour', 'minute'])
-
-        appSettings.setNumber("hour", 9)
-        appSettings.setNumber("minute", 25)
-        setAlarmComponent.configureTime(timePicker);
-
-        expect(timePicker.hour).toEqual(9);
-        expect(timePicker.minute).toEqual(25);
-    });
-
-    it("is able to save a set time value", function() {
-
-      
-    });
+  // it("is able to save a set time value", function() {
+  //   var timePicker = jasmine.createSpyObj('timePicker', ['hour', 'minute']);
+  //   timePicker.hour = 12;
+  //   timePicker.minute = 10;
+  //   setAlarmComponent.saveTime(timePicker);
+  //   expect(appSettings.getNumber("hour")).toEqual(12);
+  //   expect(appSettings.getNumber("minute")).toEqual(10);
+  // });
 });
