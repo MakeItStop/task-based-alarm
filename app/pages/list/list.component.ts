178250764bf366c8a94215e7e218ac36610d2925
Luke Cartledge <luke@lukecartledge.com>
2016-09-08 09:56:07 +0100

    adds some moment goodness

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -9,6 +9,11 @@ import * as moment from "moment";
 })
 export class ListPage {
 
-    alarmTime = applicationSettings.getNumber("hour") + ":" + applicationSettings.getNumber("minute");
+  private _now = moment().get('h') + ":" + moment().get('m');
+  private _until;
+
+  private _timeString = applicationSettings.getNumber("hour") + ":" + applicationSettings.getNumber("minute");
+
+  alarmTime = moment(this._timeString, "HH:mm").format("HH:mm");
 
 }
