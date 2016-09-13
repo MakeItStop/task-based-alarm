cbe704ad7bc664bf5bf67f9260f9335574958c27
Richard Bates <richo225@gmail.com>
2016-09-13 11:04:44 +0100

    Rename alarm to tap. Add spy for home router

---
+++ app/tests/tap.component_spec.js
@@ -0,0 +1,37 @@
+var reflect = require("reflect-metadata");
+var component = require("../pages/tap/tap.component");
+
+describe("Tests for /pages/tap/tap.component.ts", function() {
+
+  var tapComponent;
+  beforeEach(function() {
+     tapComponent = new component.TapPage();
+  });
+
+  it("Verify default message", function() {
+    expect(tapComponent.message).toBe("16 taps left");
+  });
+
+  it("Decrease the tap count", function() {
+    tapComponent.onTap();
+    expect(tapComponent.message).toBe("15 taps left");
+  });
+
+  it("Verify completed task message", function(){
+    var n = 16;
+    while (n--){
+      tapComponent.onTap();
+    };
+    expect(tapComponent.message).toBe("You are awake");
+  });
+
+  it("Routes to home after task completion", function(){
+    spyOn(tapComponent, 'routeToHome');
+    var n = 16;
+    while (n--){
+      tapComponent.onTap();
+    };
+    tapComponent.onTap();
+    expect(tapComponent.routeToHome).toHaveBeenCalled();
+  })
+})
