a4c1b8746d105586960802f6ef063f30f0b9b738
Richard Bates <richo225@gmail.com>
2016-09-14 15:14:23 +0100

    Adds alarm sound picker and default/saved sound

--- app/pages/settings/settings.component.ts
+++ app/pages/settings/settings.component.ts
@@ -5,6 +5,7 @@ import { Router } from "@angular/router";
 import { Page } from "ui/page";
 
 let taskList = ["tap","math-game","slider", "gesture", "memory"]
+let soundList = ["Foghorn","Alarm", "Bomb_Siren", "Railroad", "Warning"]
 
 @Component({
   selector: "settings",
@@ -13,15 +14,22 @@ let taskList = ["tap","math-game","slider", "gesture", "memory"]
 
 export class SettingsPage {
   private _currentTask = "";
-  public tasks: Array<string>;
-  public savedTask = applicationSettings.getString("task")
+  private _currentSound = "";
 
+  public tasks: Array<string>;
+  public sounds: Array<string>;
+
+  public savedTask = applicationSettings.getString("task");
+  public savedSound = applicationSettings.getString("sound");
 
   constructor(private _router: Router) {
     this.tasks = [];
-
     for (let i = 0; i < taskList.length; i++) {
       this.tasks.push(taskList[i]);
+    }
+    this.sounds = [];
+    for (let i = 0; i < soundList.length; i++) {
+      this.sounds.push(soundList[i]);
     }
   }
 
@@ -29,13 +37,23 @@ export class SettingsPage {
     picker.selectedIndex = picker.items.indexOf(this.savedTask)
   }
 
+  public configureSound(soundPicker: ListPicker) {
+    soundPicker.selectedIndex = soundPicker.items.indexOf(this.savedSound)
+  }
+
   public selectedIndexChanged(taskPicker) {
     this._currentTask = taskList[taskPicker.selectedIndex] || "tap";
     this._storeString("task", this._currentTask);
   }
 
+  public selectedSoundIndexChanged(soundPicker) {
+    this._currentSound = soundList[soundPicker.selectedIndex] || "Alarm";
+    this._storeString("sound", this._currentSound);
+  }
+
   private _storeString(attribute, value) {
     applicationSettings.setString(attribute, value);
+    console.log("THIS>>>>>" + applicationSettings.getString("sound"));
   }
 
   public saveSettings() {
